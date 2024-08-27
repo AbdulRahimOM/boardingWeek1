2ddb13b87bd57c8a56f5fab1da52686613c3eebc
Abdul Rahim Ovingal <abdulearningit@gmail.com>
2024-08-27 14:04:14 +0530

    editted

--- main.go
+++ main.go
@@ -19,26 +19,8 @@ type request struct {
 	P   string `json:"p"`
 	L   string `json:"l"`
 	Sc  string `json:"sc"`
-
-	Atrk1 string `json:"atrk1"`
-	Atrv1 string `json:"atrv1"`
-	Atrt1 string `json:"atrt1"`
-
-	Atrk2 string `json:"atrk2"`
-	Atrv2 string `json:"atrv2"`
-	Atrt2 string `json:"atrt2"`
-
-	Uatrk1 string `json:"uatrk1"`
-	Uatrv1 string `json:"uatrv1"`
-	Uatrt1 string `json:"uatrt1"`
-
-	Uatrk2 string `json:"uatrk2"`
-	Uatrv2 string `json:"uatrv2"`
-	Uatrt2 string `json:"uatrt2"`
-
-	Uatrk3 string `json:"uatrk3"`
-	Uatrv3 string `json:"uatrv3"`
-	Uatrt3 string `json:"uatrt3"`
+	
+	Attrs  map[string]string `json:"-"`
 }
 
 type response struct {
@@ -55,15 +37,15 @@ type response struct {
 	Traits          map[string]trait     `json:"traits"`
 }
 
-type trait struct {
-	Value string `json:"value"`
-	Type  string `json:"type"`
-}
+
 type attribute struct {
 	Value string `json:"value"`
 	Type  string `json:"type"`
 }
-
+type trait struct {
+	Value string `json:"value"`
+	Type  string `json:"type"`
+}
 func helloPostHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(w, "Method is not supported.", http.StatusMethodNotAllowed)
@@ -109,31 +91,44 @@ func worker(req request, wg *sync.WaitGroup, w http.ResponseWriter) {
 		PageTitle:       req.T,
 		PageUrl:         req.P,
 		BrowserLanguage: req.L,
-		ScreenSize:      req.Sc,
-		Attributes: map[string]attribute{
-			req.Atrk1: {
-				Value: req.Atrv1,
-				Type:  req.Atrt1,
-			},
-			req.Atrk2: {
-				Value: req.Atrv2,
-				Type:  req.Atrt2,
-			},
-		},
-		Traits: map[string]trait{
-			req.Uatrk1: {
-				Value: req.Uatrv1,
-				Type:  req.Uatrt1,
-			},
-			req.Uatrk2: {
-				Value: req.Uatrv2,
-				Type:  req.Uatrt2,
-			},
-			req.Uatrv3: {
-				Value: req.Uatrv3,
-				Type:  req.Uatrt3,
-			},
-		},
+		// ScreenSize:      req.Sc,
+		// Attributes: map[string]attribute{
+		// 	req.Atrk1: {
+		// 		Value: req.Atrv1,
+		// 		Type:  req.Atrt1,
+		// 	},
+		// 	req.Atrk2: {
+		// 		Value: req.Atrv2,
+		// 		Type:  req.Atrt2,
+		// 	},
+		// },
+		// Traits: map[string]trait{
+		// 	req.Uatrk1: {
+		// 		Value: req.Uatrv1,
+		// 		Type:  req.Uatrt1,
+		// 	},
+		// 	req.Uatrk2: {
+		// 		Value: req.Uatrv2,
+		// 		Type:  req.Uatrt2,
+		// 	},
+		// 	req.Uatrv3: {
+		// 		Value: req.Uatrv3,
+		// 		Type:  req.Uatrt3,
+		// 	},
+		// },
+	}
+	for k, v := range req.Attrs {
+		if k[:4] == "atrk" {
+			resp.Attributes[v] = attribute{
+				Value: req.Attrs[k[0:3] + "v" + k[4:]],
+				Type:  req.Attrs[k[0:3] + "t" + k[4:]],
+			}
+		}else if k[:5] == "uatrk" {
+			resp.Traits[v] = trait{
+				Value: req.Attrs[k[0:4] + "v" + k[5:]],
+				Type:  req.Attrs[k[0:4] + "t" + k[5:]],
+			}
+		}
 	}
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(resp)
